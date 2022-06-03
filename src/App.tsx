25c5aae78fc2222129e458b8dc57302cbc2cd504
rregalado17 <rregalado17@gmail.com>
2022-06-03 18:51:39 -0500

    beginner code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let name: string;
+let age: number;
+let isStudent:boolean;
+let hobbies:string[]; 
+//tuple contains a fix amount of values and types. the example below can contain 1 number and 1 string.
+let role:[number, string];
+//How to define objects? Do not declare object as a type since it can contain multiple types within that object.
+type Person= {
+  name:string;
+  age: number | string; //defines it as number or string
+  //age?: number;
+}
+let person: Person = {
+  name: "Rob",
+  age: 30,
+  //if declared in the type above without a "?", the type is required for object initiation. 
+}
+//Assign the types below is multiple groups
+let lotsOfPeople: Person[];
+
+//How do we assign functions?
+// let printName: (name:string) => void;
+
+function printName(name: string) {
+  console.log(name)
+}
+printName("Rob")
+
+//If you want to provide any type, use keyword any.
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World.
     </div>
   );
 }
